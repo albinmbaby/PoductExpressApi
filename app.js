5693fd329e0eb71ffb5697cae43fad570cd91002
albinmbaby <albinmbaby@gmail.com>
2024-11-06 06:59:04 +0530

    Second commit

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express()
 app.use(express.json())
 app.use('/products',productRouter)
 
+app.get('/',(req,res)=>{
+    res.send("Hello world")
+})
+
 app.get('/home',(req,res)=>{
     // console.log(__dirname)
     res.sendFile(path.join(__dirname,"/index.html"))
